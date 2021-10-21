3c6547b8ffe4464a0955df6a2de77568c5f35bbe
Leonardo Almeida <leonardo.almeida18@gmail.com>
2021-10-21 15:05:30 -0300

    feat: add split components Content and SideBar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
+import '../styles/content.scss';
+
+import { MovieCard } from "./MovieCard";
+import { MovieProps, GenreResponseProps } from '../App';
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export function Content({ selectedGenre, movies }: ContentProps) {
+ return (
+  <div className="container">
+  <header>
+    <span className="category">
+      Categoria:<span> {selectedGenre.title}</span>
+    </span>
+  </header>
+
+  <main>
+    <div className="movies-list">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.imdbID}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))}
+    </div>
+  </main>
+</div>
+ )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import "../styles/sidebar.scss";
+import { Button } from './Button';
+import { GenreResponseProps } from '../App'
+
+interface SideBarProps {
+  handleClickButton: any;
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+}
+
+export function SideBar({ handleClickButton, genres, selectedGenreId }: SideBarProps) {
+  
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
